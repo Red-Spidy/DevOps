7389c40525fd754d4fd37d4f65e628350917469a
EC2 Default User <ec2-user@ip-172-31-36-69.ec2.internal>
2026-05-03 14:48:27 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+const VERSION = "v2";
+
+app.use(express.static(__dirname));
+
+app.get('/api/status', (req, res) => {
+    res.json({
+        status: "Running",
+        version: VERSION,
+        time: new Date().toLocaleString("en-IN", {
+timeZone: "Asia/Kolkata"}),
+        uptime: process.uptime().toFixed(2) + " sec"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
